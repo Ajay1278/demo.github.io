68211eee1cfe3a2b6d03c71a4ba9a7be431836b4
Ajay Ratnaparkhi <110182312+Ajay1278@users.noreply.github.com>
2023-04-25 20:37:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
+import gangaain from './assets/gangaain-logo.png
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -13,7 +14,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={gangaain} className="gangaain" alt="gangaain" />
         </a>
       </div>
       <h1> hello , i change something</h1>
